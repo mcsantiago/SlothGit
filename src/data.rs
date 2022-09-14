06a9867594d150a2d0f51986a94b9ee172690e80
Matthew Santiago <carag.matthew@gmail.com>
2022-09-13 23:46:06 -0500

    Adding basic hash-object implementation

--- src/data.rs
+++ src/data.rs
@@ -1,12 +1,47 @@
 use std::fs;
 use std::env;
+use sha1::{Sha1, Digest};
+
+const SLOTH_DIR: &str = "./.slothgit";
+const OBJECTS_DIR: &str = "./.slothgit/objects";
 
 pub fn initialize() -> std::io::Result<()> {
-    // Remember, the binary is not where the pwd is on the console
     let path = env::current_dir()?;
-    match fs::create_dir("./.slothgit") {
+    match fs::create_dir(SLOTH_DIR) {
         Ok(_) => println!("Initialized empty SlothGit repository in {}/.slothgit", path.display()),
         Err(_) => println!("Reinitialized existing SlothGit repository in {}/.slothgit", path.display())
     };
+    match fs::create_dir(OBJECTS_DIR) {
+        Ok(_) => (),
+        Err(_) => println!("Error creating {}/.slothgit/objects", path.display())
+    };
     Ok(())
 }
+
+pub fn store_hash_object(file: Option<String>, writeToDb: bool) -> std::io::Result<()> {
+    let mut hasher = Sha1::new();
+
+    match file {
+        Some(file_path) => {
+            let contents: String = fs::read_to_string(file_path).expect("Unable to read file {file_path}");
+            hasher.update(&contents);
+            let oid = hasher.finalize();
+            let output_path = format!("{}/{:#01x}", OBJECTS_DIR, oid);
+            if writeToDb {
+                match fs::create_dir(OBJECTS_DIR) {
+                    Ok(_) => (),
+                    Err(_) => (),
+                };
+                match fs::write(&output_path, contents) {
+                    Ok(_) => (),
+                    Err(e) => println!("Error writing to {}: {}", output_path, e),
+                }
+            };
+            println!("{:#01x}", oid)
+        },
+        None => { () }
+    }
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,10 @@ fn display_help_menu() -> std::io::Result<()> {
 fn main() {
     // Parse out the command. If no command exists, display the help menu
     let command : Option<String> = env::args().nth(1);
+    let operand : Option<String> = env::args().nth(2);
     let _result: Result<(), std::io::Error> = match command.as_ref().map(String::as_ref) {
         Some("init") => data::initialize(),
+        Some("hash-object") => data::store_hash_object(operand, true),
         Some("--help") | Some("help") => display_help_menu(),
         _  => display_help_menu(),
     };
